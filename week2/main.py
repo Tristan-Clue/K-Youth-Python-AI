32f0339461112a57ad460219f14f833229fb8a5c
Tristan <123tristanma@gmail.com>
2026-06-19 09:25:38 +0800

    chore: Added function for day3-4

--- week2/main.py
+++ week2/main.py
@@ -1,11 +1,21 @@
 from src.tag_data import tag_data
+from src.find_skill_gap import find_skill_gaps
 
 def main():
-    try:
-        tag_data("data/jobs_d1.db")
-    except Exception as e:
-        print(f"Something went wrong: {e}")
+    ## DAY 1-2 : Tag_data
+    # try:
+    #     tag_data("data/jobs_d1.db")
+    # except Exception as e:
+    #     print(f"Something went wrong: {e}")
 
+    ## DAY 3-4 : Find Skill gap
+    input_file_path = "data/resume_d3_eval.txt"
+    db_url = "data/jobs_d3_eval.db"
+    gaps = find_skill_gaps(input_file_path, db_url)
+    print(gaps)
+    
+
+    ## Testing for prompt model
     # if (len(sys.argv) == 3):
     #     try:
     #         print("calling function")
