673e2956ce756dfbab8efd6bf8570cc3f1639232
Tristan <123tristanma@gmail.com>
2026-06-24 17:04:46 +0800

    feat: Added a  mount for static directory and set up testing.

--- week3/frontend/src/app.py
+++ week3/frontend/src/app.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Creates web server object
 app = FastAPI()
@@ -11,6 +17,16 @@ app = FastAPI()
 base_dir = Path(__file__).resolve().parent
 templates = Jinja2Templates(base_dir / "templates")
 
+
+app.mount(
+"/static",
+StaticFiles(directory=base_dir / "static"),
+name="static"
+)
+
+class ChatRequest(BaseModel):
+    message: str
+
 # Registers route inside app; Creates  GET/ route
 # Decorator, attaches behavior to functions. Like events?
 # Tells FastAPI: "When someone sents a HTTP GET request to this route, run this function"
@@ -19,5 +35,15 @@ templates = Jinja2Templates(base_dir / "templates")
 def home(request: Request):
     return templates.TemplateResponse(
         request=request,
-        name="chat_page.html"
+        name="chat_page.html",
+        # Passing context (BACKEND_URL) to HTML
+        context={"backend_url": os.getenv("BACKEND_URL", "http://localhost:8001")}
     )
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    
+
+    reply = f"You said: {request.message}"
+
+    return {"reply": reply}
